663f965111ced2a4b0645a6392c78e017b3b8689
Andrei137 <andrei.fabian188@gmail.com>
2025-10-18 20:51:10 +0300

    [CI/CD] Publish npm module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export default {};
+import { readFileSync } from 'fs';
+import { parse } from 'dotenv';
+
+type Config = Record<string, string> & {
+  add: (...keys: string[]) => Config;
+}
+
+const config: Config = parse(readFileSync('./.env', 'utf-8')) as Config;
+
+config.add = function (this: Config, ...keys: string[]) {
+    for (const key of keys) {
+        if (process.env[key] !== undefined) {
+            this[key] = process.env[key]!;
+        }
+    }
+    return this;
+};
+
+config.add('NODE_ENV')
+
+export default config;
